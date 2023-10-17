171575a6dd061fb60b42f3f59b1cebffa238acc9
GeorgeAdemba <georgeade.47@gmail.com>
2023-10-17 12:43:46 +0300

    Create day_16.py

---
+++ George_Ademba/day_16.py
@@ -0,0 +1,4 @@
+inventory={"item1":{"name":"pens","quantity":50},"item2":{"name":"books","quantity":25}}
+inventory["item1"]["quantity"]=30
+inventory["item2"]["quantity"]=20
+print("Inventory Updated",inventory)
